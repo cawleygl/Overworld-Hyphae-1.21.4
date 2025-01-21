f74b6792fa37da153cc0d74e6df48094f809624d
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-01-21 17:14:32 -0500

    Initial commit

---
+++ src/main/java/bluesteel42/overworldhyphae/OverworldHyphaeClient.java
@@ -0,0 +1,10 @@
+package bluesteel42.overworldhyphae;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OverworldHyphaeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

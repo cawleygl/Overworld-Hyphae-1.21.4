9f28a1535cf3d4a9e25e8600a3284a3bc3f1d0c2
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-01-21 21:23:07 -0500

    feat: added mushroom hyphae

--- src/main/java/bluesteel42/overworldhyphae/block/ModBlocks.java
+++ src/main/java/bluesteel42/overworldhyphae/block/ModBlocks.java
@@ -16,32 +16,29 @@ import net.minecraft.util.math.Direction;
 import java.util.function.Function;
 
 public class ModBlocks {
-    public static final Block MUSHROOM_STEM = registerModBlock("mushroom_stem", PillarBlock::new, AbstractBlock.Settings.create()
+
+    public static final Block MUSHROOM_HYPHAE = register("mushroom_hyphae", PillarBlock::new, AbstractBlock.Settings.create()
+            .mapColor(MapColor.WHITE_GRAY)
+            .instrument(NoteBlockInstrument.BASS)
+            .strength(0.2F)
+            .sounds(BlockSoundGroup.WOOD)
+            .burnable());
+
+    public static final Block MUSHROOM_STEM = register("mushroom_stem", PillarBlock::new, AbstractBlock.Settings.create()
             .mapColor(state -> state.get(PillarBlock.AXIS) == Direction.Axis.Y ? MapColor.OAK_TAN : MapColor.WHITE_GRAY)
             .instrument(NoteBlockInstrument.BASS)
             .strength(0.2F)
             .sounds(BlockSoundGroup.WOOD)
             .burnable());
 
-    public static final Block MUSHROOM_PLANKS = registerModBlock("mushroom_planks", Block::new, AbstractBlock.Settings.create()
+    public static final Block MUSHROOM_PLANKS = register("mushroom_planks", Block::new, AbstractBlock.Settings.create()
             .mapColor(MapColor.OAK_TAN)
             .instrument(NoteBlockInstrument.BASS)
             .strength(0.2F)
             .sounds(BlockSoundGroup.WOOD)
             .burnable());
 
-    private static Block registerMinecraftBlock(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
-        final Identifier identifier = Identifier.ofVanilla(path);
-
-        final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
-
-        final Block block = Blocks.register(registryKey, factory, settings);
-        Items.register(block);
-
-        return block;
-    }
-
-    private static Block registerModBlock(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+    private static Block register(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
         final Identifier identifier = Identifier.of(OverworldHyphae.MOD_ID, path);
 
         final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
@@ -53,17 +50,20 @@ public class ModBlocks {
 
     }
 
+
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL)
                 .register((itemGroup) -> {
-                    itemGroup.addAfter(Items.MUSHROOM_STEM, Items.BROWN_MUSHROOM_BLOCK);
-                    itemGroup.addAfter(Items.MUSHROOM_STEM, Items.RED_MUSHROOM_BLOCK);
+                    itemGroup.addAfter(Items.PALE_OAK_LOG, Items.BROWN_MUSHROOM_BLOCK);
+                    itemGroup.addAfter(Items.PALE_OAK_LOG, Items.RED_MUSHROOM_BLOCK);
+                    itemGroup.addAfter(Items.PALE_OAK_LOG, ModBlocks.MUSHROOM_STEM);
                 }
         );
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS)
                 .register((itemGroup) -> {
                             itemGroup.addAfter(Items.BAMBOO_BUTTON, ModBlocks.MUSHROOM_PLANKS);
-                            itemGroup.addAfter(Items.BAMBOO_BUTTON, Items.MUSHROOM_STEM);
+                            itemGroup.addAfter(Items.BAMBOO_BUTTON, ModBlocks.MUSHROOM_HYPHAE);
+                            itemGroup.addAfter(Items.BAMBOO_BUTTON, ModBlocks.MUSHROOM_STEM);
                         }
                 );
     }
